c821e20482e314148ba8f74c9450bb4f7b34b96c
rrrokonislam-rgb <rrrokonislam@gmail.com>
2026-07-06 03:08:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from telethon.errors import SessionPasswordNeededError
 # ==================== CORE CONFIGURATION ====================
 API_ID = 36547444
 API_HASH = "119a3ac4fd3dc368df92ae6d81f3bb3e"
-BOT_TOKEN = "8288574083:AAGjtwBIlm-gYLHm5DJIUvlN4ThNO1dTRlg"
+BOT_TOKEN = "8288574083:AAEXj4EbchN-piQn6w-0Xsl2x6eKhuiDc7s"
 ADMIN_ID = 8095751648
 
 MAIN_CHANNEL_ID = -1003904729385          
